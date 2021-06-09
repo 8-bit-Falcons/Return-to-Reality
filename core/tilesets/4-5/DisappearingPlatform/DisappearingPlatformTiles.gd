acf845bb46e5e8806e082287cd94de17192f92ca
sloshyyy <nate@jankowskis.net>
2021-06-09 15:37:48 -0400

    Added comments to the tileset code
    It wasn't exactly straightforward so I wanted to explain it

--- core/tilesets/4-5/DisappearingPlatform/DisappearingPlatformTiles.gd
+++ core/tilesets/4-5/DisappearingPlatform/DisappearingPlatformTiles.gd
@@ -1,12 +1,14 @@
 extends TileMap
 
-const dt = preload("res://scenes/misc/DisappearingPlatform/DisappearingPlatform.tscn")
+const platform_scene = preload("res://scenes/misc/DisappearingPlatform/DisappearingPlatform.tscn")
 
 
 func _ready():
-	var disappearing_tiles = get_used_cells_by_id(0)
+	var disappearing_tiles = get_used_cells_by_id(0)  
+	# gets an array of cells in the tilemap which contain the tile with an index of zero in the tile set
 	for tile in disappearing_tiles:
-		var new_tile = dt.instance()
-		new_tile.set_position(map_to_world(tile))
-		set_cell(tile.x, tile.y, -1)
-		self.add_child(new_tile)
+		var new_tile = platform_scene.instance()  # gets a new instance of the scene, including child nodes
+		new_tile.set_position(map_to_world(tile))  
+		# Gets the position of the placeholder tile and sets the scene position to that
+		set_cell(tile.x, tile.y, -1)  # clears the tile
+		self.add_child(new_tile)  # puts an instance of the scene at the same location
