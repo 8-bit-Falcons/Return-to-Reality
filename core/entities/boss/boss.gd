8374c10d3db2761cfb720f2781d1354e02af11bd
sloshyyy <nate@jankowskis.net>
2021-06-10 11:10:42 -0400

    Dying works
    fades from existence and has a death signal

--- core/entities/boss/boss.gd
+++ core/entities/boss/boss.gd
@@ -1,12 +1,17 @@
 extends KinematicBody2D
 
+signal dying
+
 const TIME_BETWEEN_HITS = 0.5  # seconds
 # Last number is how many flashes; this ensures that flashes are consistent & divisible
 const FLASH_FREQ = TIME_BETWEEN_HITS / 5
 # normal color; damage color
 const COLOR_DAMAGE = ["ffffff", "ff9c9c"]
+const MINIMUM_HEALTH = 0 # death health
+const FADE_RATE = 0.1  # value between 0 and 1 for how much to fade per frame
 
-var health = 3
+var health = 2
+var dead = false  # this isn't really needed but I added it for simplicity's sake
 var time_since_hit = 0.0
 var time_since_flash = 0.0
 var damagecolor_active: bool = false
@@ -16,6 +21,10 @@ func _ready():
 	pass
 	#$AnimatedSprite.play("summoned")
 	
+func _physics_process(delta):
+	if dead:
+		$AnimatedSprite.modulate.a -= FADE_RATE
+
 func _process(delta):
 	if time_since_hit > 0.0:
 		time_since_hit += delta
@@ -32,8 +41,16 @@ func change_color(active: bool):
 	damagecolor_active = active
 	$AnimatedSprite.modulate = Color(COLOR_DAMAGE[int(damagecolor_active)])
 
+func die():
+	dead = true
+	emit_signal("dying")
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Area2D/TophatHitbox.set_deferred("disabled", true)
+
 func _on_Area2D_body_entered(body):
 	if time_since_hit == 0.0:
-		time_since_hit = 0.001
 		health -= 1
-		print('ouch. %f health' % health)
+		if health >= MINIMUM_HEALTH:
+			time_since_hit = 0.001
+		else:
+			die()
