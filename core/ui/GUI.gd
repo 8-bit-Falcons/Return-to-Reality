40c0db3280939313ba958e930fcdd23b97ec97fd
Cierra <cc362x@gmail.com>
2021-07-04 01:22:58 -0400

    added file to save settings

--- core/ui/GUI.gd
+++ core/ui/GUI.gd
@@ -2,10 +2,12 @@ extends CanvasLayer
 
 enum {NONE, PAUSE_MENU, SETTINGS_MENU} # Defines menus
 enum {RESUME, SETTINGS, MAIN_MENU} # Defines pause menu buttons
-enum {VOLUME0, VOLUME1, VOLUME2, VOLUME3} # Defines settings menu buttons
+enum {VOLUME0 = -80, VOLUME1 = -40, VOLUME2 = -20, VOLUME3 = 0} # Defines settings menu buttons
 var current_menu = NONE # The menu currently open
 var pause_selected = RESUME # The button selected on the pause menu
-var settings_selected = VOLUME3 # The button selected on the settings menu
+var settings_file = "res://settings.save"
+var settings_selected = load_settings() # The button selected on the settings menu
+
 
 # When the pause button is pressed, inverse the pause state of the level
 func _input(event):
@@ -60,10 +62,19 @@ func _process(delta):
 			$PauseMenu.set_visible(new_pause_state)
 			current_menu = NONE
 			PauseMusic.stop()
-		# Pressing ui_accept when the settings button is selected opens a popup
+		# Pressing ui_accept when the settings button is selected opens the
+		# settings menu
 		if Input.is_action_just_pressed("ui_accept") and pause_selected == SETTINGS:
 			$SettingsMenu.set_visible(true)
 			current_menu = SETTINGS_MENU
+			if settings_selected == VOLUME0:
+				$SettingsMenu/Volume0.show()
+			elif settings_selected == VOLUME1:
+				$SettingsMenu/Volume1.show()
+			elif settings_selected == VOLUME2:
+				$SettingsMenu/Volume2.show()
+			else:
+				$SettingsMenu/Volume3.show()
 		# Pressing ui_accept when the main menu button is selected returns us to
 		# the main menu
 		if Input.is_action_just_pressed("ui_accept") and pause_selected == MAIN_MENU:
@@ -72,6 +83,7 @@ func _process(delta):
 			$PauseMenu.set_visible(new_pause_state)
 			current_menu = NONE
 			Music.stop()
+			PauseMusic.stop()
 			get_tree().change_scene("res://ui/main/MainMenu.tscn")
 	
 	# On the settings menu, pressing the arrow keys changes the volume
@@ -81,30 +93,51 @@ func _process(delta):
 				settings_selected = VOLUME0
 				$SettingsMenu/Volume1.hide()
 				$SettingsMenu/Volume0.show()
-				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -80)
+				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), set_volume(VOLUME0))
 			elif settings_selected == VOLUME2:
 				settings_selected = VOLUME1
 				$SettingsMenu/Volume2.hide()
 				$SettingsMenu/Volume1.show()
-				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -40)
+				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), set_volume(VOLUME1))
 			elif settings_selected == VOLUME3:
 				settings_selected = VOLUME2
 				$SettingsMenu/Volume3.hide()
 				$SettingsMenu/Volume2.show()
-				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -20)
+				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), set_volume(VOLUME2))
 		if Input.is_action_just_pressed("ui_right"):
 			if settings_selected == VOLUME0:
 				settings_selected = VOLUME1
 				$SettingsMenu/Volume0.hide()
 				$SettingsMenu/Volume1.show()
-				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -40)
+				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), set_volume(VOLUME1))
 			elif settings_selected == VOLUME1:
 				settings_selected = VOLUME2
 				$SettingsMenu/Volume1.hide()
 				$SettingsMenu/Volume2.show()
-				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -20)
+				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), set_volume(VOLUME2))
 			elif settings_selected == VOLUME2:
 				settings_selected = VOLUME3
 				$SettingsMenu/Volume2.hide()
 				$SettingsMenu/Volume3.show()
-				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 0)
+				AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), set_volume(VOLUME3))
+				
+func save_settings():
+	var f = File.new()
+	f.open(settings_file, File.WRITE)
+	f.store_var(settings_selected)
+	f.close()
+
+func load_settings():
+	var f = File.new()
+	if f.file_exists(settings_file):
+		f.open(settings_file, File.READ)
+		settings_selected = f.get_var()
+		f.close()
+	else:
+		settings_selected = VOLUME3
+	return settings_selected
+		
+func set_volume(volume):
+	settings_selected = volume
+	save_settings()
+	return volume
